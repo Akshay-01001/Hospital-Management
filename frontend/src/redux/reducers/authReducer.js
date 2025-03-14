1fffb1bcce0e49a99ec78225cef51877d9d49f3e
Akshay-01001 <akshaypatel5048@gmail.com>
2025-03-14 11:23:37 +0530

    added redux store and basic dashboard ui

---
+++ frontend/src/redux/reducers/authReducer.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null, // Renamed `err` to `error` for consistency
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess: (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    loginFailure: (state, action) => {
+      state.user = null;
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.loading = false;
+      state.error = null;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  authSlice.actions;
+export default authSlice.reducer;
